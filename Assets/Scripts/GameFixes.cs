def7be747f83c09cef2d6c406a29826798948d4d
Qkrisi <qkrisiyt@gmail.com>
2022-01-29 08:23:39 +0100

    Make sure selectable components have the ModSelectable component (UpdateChildrenProperly)
    CopySettingsFromProxy adds ModHighlightable

--- Assets/Scripts/GameFixes.cs
+++ Assets/Scripts/GameFixes.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Reflection;
+using UnityEngine;
 using KModkit;
 
 //Methods for working around bugs in the game
@@ -25,7 +26,7 @@ public static class GameFixes
         var call = false;
         return () =>
         {
-            call = !call;
+            call ^= true;
             if (call)
                 action();
         };
@@ -67,7 +68,9 @@ public static class GameFixes
     public static void UpdateSettings(this KMSelectable selectable)
     {
         if (CopySettingsFromProxyMethod != null)
-            CopySettingsFromProxyMethod.Invoke(selectable.GetComponent(ModSelectableType), new object[0]);
+            CopySettingsFromProxyMethod.Invoke(
+                selectable.GetComponent(ModSelectableType) ?? selectable.gameObject.AddComponent(ModSelectableType),
+                new object[0]);
     }
     
 #endregion
@@ -77,8 +80,9 @@ public static class GameFixes
     static GameFixes()
     {
         ModSelectableType = ReflectionHelper.FindGameType("ModSelectable");
-        CopySettingsFromProxyMethod =
-            ModSelectableType.GetMethod("CopySettingsFromProxy", BindingFlags.Public | BindingFlags.Instance);
+        if (ModSelectableType != null)
+            CopySettingsFromProxyMethod =
+                ModSelectableType.GetMethod("CopySettingsFromProxy", BindingFlags.Public | BindingFlags.Instance);
     }
 #endif
 }
